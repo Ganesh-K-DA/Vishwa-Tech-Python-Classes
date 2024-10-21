693461d9f395af3c014d567d4da2e2386915fccd
Ganesh-K-DA <ganeshkulagi123@gmail.com>
2024-10-21 13:01:05 +0530

    Update sandhya.py

--- sandhya.py
+++ sandhya.py
@@ -1,1 +1,56 @@
-hi welcome to the git class
+hi welcome
+this is a very biginning class of a git
+today we created a git hub accout and downloaded git hub account and cli account
+we ran few commants that today 
+
+ mkdir python-classes
+    2  cd python-classes
+    3  git config --global user.name
+    4  git config --global user.name "Ganesh-K-DA"
+    5  git config --global user.email "ganeshkulagi123@gmail.com"
+    6  git config --list
+    7  git init
+    8  cp -R C:\Users\Ganesh.Kulagi\OneDrive\Desktop\practice folder\* .
+    9  ls
+   10  pwd
+   11  ls
+   12  git status
+   13  git add .
+   14  git status
+   15  git commit -m "ganesh first commit"
+   16  git status
+   17  git branch -M main
+   18  git remote add origin https://github.com/Ganesh-K-DA/Vishwa-Tech-Python-Classes.git
+   19  git remote -v
+   20  git push -u origin main
+   21  touch da.py
+   22  git status
+   23  git add
+   24  git add .
+   25  git status
+   26  git commit -m "this is my second commit"
+   27  git push
+   28  ls
+   29  git pull
+   30  ls
+   31  touch sandhya.py
+   32  touch shantha.py
+   33  status
+   34  git status
+   35  git add
+   36  git add .
+   37  git status
+   38  git commit
+   39  git commit -m "this is my third commit"
+   40  git push
+   41  git log
+   42  git show cb4bf452fcb3cd3e00574d58ad3f2449df3c22d1
+   43   git show 0a5cb77bf7b3e5f24340f0fe0757bbf6e2662b78
+   44  echo "hi welcome to the git class" > sandhya.py
+   45  cat sandhya.py
+   46  git add
+   47  git add .
+   48  git commit
+   49  git commit -m "some changes made in file"
+   50  git push
+
